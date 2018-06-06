6e2f3f93bd4ee41323245519e6a27de4560a75d6
kurepark <eunhye0406@gmail.com>
2018-06-06 22:16:12 +0900

    es6문법에 맞게 정리/ 뷰 그리는 코드를 각 컴포넌트별로 분리

--- week8-study/src/index.js
+++ week8-study/src/index.js
@@ -1,17 +1,18 @@
-const $ = require('jquery');
+import $ from 'jquery';
+import {Button, Div} from './components'; //폴더명만 쓰면 index.js를 읽어옴
 
 const app = $('#app');
-app.append(`<div id="count">0</div>`);
-app.append(`<button id="btn-up">up</button>`);
-app.append(`<button id="btn-down">down</button>`);
+app.append(Div({id:'count', children:0}));
+app.append(Button({id:'btn-up', children: 'up'}));
+app.append(Button({id:'btn-down', children: 'down'}));
 
 const model = {
     count : 0
 }
 
-$("#btn-up").on('click',function () {
+$("#btn-up").on('click', () => {
    $("#count").text(++model.count);
 });
-$("#btn-down").on('click',function () {
+$("#btn-down").on('click', () => {
    $("#count").text(--model.count);
 });
