59449e92407277a708ef6bb69ef6567ffda2f672
kurepark <eunhye0406@gmail.com>
2018-06-24 10:23:30 +0900

    10주차 과제 해지하기버튼 풀이

--- week10-study/src/components/membership/Trial.js
+++ week10-study/src/components/membership/Trial.js
@@ -1,56 +1,16 @@
 import React from 'react';
 import ContentBox from './ContetnBox';
-import Modal from 'react-modal';
 
-const customStyles = {
-    content : {
-        top :'50%',
-        left: '50%',
-        right : 'auto',
-        bottom : 'auto',
-        marginRight : '-50%',
-        transform : 'translate(-50%,-50%)'
-    }
-};
-
-Modal.setAppElement('#app');
 export default class Trial extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            isOpen : false,
-            membershipLevel : super()
-        }
-        this.closeModal = this.closeModal.bind(this);
-    }
-    closeModal() {
-        this.setState({isOpen: false, membershipLevel:'guest'});
-        console.log(this.state.membershipLevel);
-        alert('무료기간 중 탈퇴가 완료되었습니다')
     }
     render() {
         return (
             <div>
                 <h1>고객님은 30일 무료회원입니다</h1>
                 <ContentBox/>
-                <button onClick={()=>{
-                    this.setState({
-                        isOpen:true
-                    })
-                }}>해지하기</button>
-                <Modal
-                    isOpen={this.state.isOpen}
-                    style={customStyles}
-                    onRequestClose={this.closeModal}
-                >
-                    <div>회원이십니다. 탈퇴하시겠습니까?</div>
-                    <button onClick={this.closeModal}>확인</button>
-                    <button onClick={()=>{
-                        this.setState({
-                            isOpen : false
-                        })
-                    }}>취소</button>
-                </Modal>
+                <button onClick={this.props.onUnRegister}>해지하기</button>
             </div>
         )
     }
--- week10-study/src/containers/MembershipHome.js
+++ week10-study/src/containers/MembershipHome.js
@@ -5,19 +5,34 @@ class MembershipHome extends React.Component {
     constructor() {
         super();
         this.state = {
-            membershipLevel : 'member'
+            membershipLevel : 'guest'
         }
+        this.onRegister = this.onRegister.bind(this);//onRegister에는 this를 바인딩 한다.
+        //onRegister의 this를 항상 멤버쉼홈의 this로 박아버림.
+        this.onUnRegister = this.onUnRegister.bind(this);
+    }
+    //클래스의 메소드로 변경
+    onRegister(){
+        alert('30일 제한으로 가입되었습니다');
+        console.log(this);//undefined
+        //함수의 this는 선언이 아니라 실행되는 방법에 의해서 결정.
+        //실행은 아래 게스트.js안에 있는 가입하기 버튼의 온클릭일때 실행.
+        //따라서 this.setState의 this가 undefined.
+        this.setState({
+            membershipLevel : 'trial'
+        });
+    }
+    onUnRegister(){
+        alert('멤버쉽이 해지되었습니다');
+        this.setState({
+            membershipLevel : 'guest'
+        });
     }
     render() {
         switch (this.state.membershipLevel) {
-            case 'guest' : return <Guest onRegister={() => {
-            alert('30일 제한으로 가입되었습니다');
-            this.setState({
-                membershipLevel : 'trial'
-            })
-            }} />;
-            case 'trial' : return <Trial/>;
-            case 'member' : return <Member/>;
+            case 'guest' : return <Guest onRegister={this.onRegister} />; //함수자체를 전달
+            case 'trial' : return <Trial onUnRegister={this.onUnRegister}  />;
+            case 'member' : return <Member onUnRegister={this.onUnRegister} />;
             default : return <Guest/>;
         }
     }
