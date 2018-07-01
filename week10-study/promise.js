a11c28f4400db469309f55e91d09d2b7d6d54cc2
kurepark <eunhye0406@gmail.com>
2018-07-01 11:27:08 +0900

    12주차 은닉화

--- week10-study/promise.js
+++ week10-study/promise.js
@@ -1,9 +1,9 @@
 
 //각 객체가 하는 역할을 묶음
-const baristar = {
+/*const baristar = {
     order(request) {
         console.log(request.name + '을 주문 받음');
-        return createCoffee(request);
+        return this.createCoffee(request);
     },
     createCoffee(request) {
         console.log(request.name + '를 만드는 중');
@@ -25,7 +25,7 @@ const baristar = {
             },2000)
         });
     }
-}
+}*/
 
 function order(request) {
     console.log(request.name + '을 주문 받음');
@@ -52,7 +52,36 @@ function createCoffee(request) {
     });
 }
 
+/* 은닉화, createCoffee는 숨긴다. return 으로 order만 반환하면 사용자는 order만 볼 수 있음*/
+const baristar = (function(){
+    function order(request) {
+        console.log(request.name + '을 주문 받음');
+        return createCoffee(request);
+    }
+    function createCoffee(request) {
+        console.log(request.name + '를 만드는 중');
 
+        return new Promise((resolve,reject) => {
+            setTimeout(()=>{
+                const coffee = {
+                    name: request.name,
+                    contents : [{
+                        name: 'espresso',
+                        cnt : 2
+                    },{
+                        name: 'milk',
+                        cnt : 1
+                    }]
+                };
+                console.log(request.name + '를 만들었다');
+                resolve(coffee);
+            },2000)
+        });
+    }
+    return {
+        order : order
+    }
+})();
 /**********/
 //각 객체가 하는 역할을 묶음
 const Iwill = {
